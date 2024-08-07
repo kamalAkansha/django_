b15eb398b62ce2280710b2b5bf02dcd86491b061
Akansha Kamal <166831619+kamalAkansha@users.noreply.github.com>
2024-08-07 22:46:31 +0530

    Add files via upload

---
+++ mysite/blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import get_object_or_404, render
+from.models import Post
+
+
+def home(request):
+
+    all_posts = Post.newmanager.all()
+
+    return render(request, 'index.html', {'posts' : all_posts})
+
+def post_single(request, post):
+
+    post = get_object_or_404(Post, slug=post, status='published')
+
+    return render(request, 'single.html', {'post' : post})
